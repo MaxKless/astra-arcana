d06da4f190d45ceb88550b86ccde4d69b3127b95
Max Kless <maxk@nrwl.io>
2025-05-27 17:32:38 +0300

    implement spellcasting

--- apps/vscode/src/extension.ts
+++ apps/vscode/src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { SpellcastingSDK } from '@astra-arcana/spellcasting-sdk';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('extension "astra-arcana" is now active!');
+  const sdk = new SpellcastingSDK();
+  const disposable = vscode.commands.registerCommand(
+    'astra-arcana.cast-spell',
+    async () => {
+      const ingredients = await sdk.getIngredients();
+      const selectedIngredients = await vscode.window.showQuickPick(
+        ingredients.map((ingredient) => ingredient.name),
+        {
+          placeHolder: 'Select an ingredient',
+          canPickMany: true,
+        }
+      );
+      if (!selectedIngredients) {
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "astra-arcana" is now active!');
+      const spell = await sdk.castSpell(selectedIngredients, []);
+      vscode.window.showInformationMessage(`Spell cast: ${spell}`);
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('astra-arcana.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Astra Arcana!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
