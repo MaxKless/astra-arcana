73d6522af118626a23ff73f3d9df3982475e4a63
Max Kless <maxk@nrwl.io>
2025-06-11 09:31:13 -0400

    reimplement vscode spellcast

--- apps/vscode/src/extension.ts
+++ apps/vscode/src/extension.ts
@@ -1,5 +1,50 @@
+import { SpellcastingSDK } from '@astra-arcana/spellcasting-sdk';
 import * as vscode from 'vscode';
 
-export async function activate(context: vscode.ExtensionContext) {}
+export async function activate(context: vscode.ExtensionContext) {
+  vscode.commands.registerCommand('astra-arcana.cast-spell', async () => {
+    const sdk = new SpellcastingSDK();
+
+    const ingredients = await sdk.getIngredients();
+
+    const selectedIngredients = await vscode.window.showQuickPick(
+      ingredients.map((i) => i.name),
+      {
+        canPickMany: true,
+        title: 'select ingredients for spell',
+      }
+    );
+
+    if (!selectedIngredients) {
+      return;
+    }
+
+    const incantations = await sdk.getIncantations();
+    const selectedIncantations = await vscode.window.showQuickPick(
+      incantations.map((i) => i.name),
+      {
+        canPickMany: true,
+        title: 'select incantations for the spell',
+      }
+    );
+
+    if (!selectedIncantations) {
+      return;
+    }
+
+    const spell = await sdk.castSpell(
+      selectedIngredients,
+      selectedIncantations
+    );
+
+    if (spell.success) {
+      vscode.window.showInformationMessage(
+        `spell cast successfully: ${spell.message}`
+      );
+    } else {
+      vscode.window.showErrorMessage(`spell failed: ${spell.message}`);
+    }
+  });
+}
 
 export function deactivate() {}
